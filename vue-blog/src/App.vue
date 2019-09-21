4634877ca3e450ee64fa8131732cf6e627b555ff
TroddenSpade <parsasam1378@gmail.com>
2019-09-21 23:48:12 +0430

    router added

--- vue-blog/src/App.vue
+++ vue-blog/src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <router-view :key="$route.fullPath"></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Header
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url("https://fonts.googleapis.com/css?family=Courgette&display=swap");
+@import url("https://fonts.googleapis.com/css?family=Anton&display=swap");
+
+* {
+  margin: 0;
+  color: white;
+}
+
+.app {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 }
 </style>
