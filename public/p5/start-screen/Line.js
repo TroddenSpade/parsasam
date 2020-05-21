cec62fad8c406ee53ebb4de0f336dd0b13d3b752
TroddenSpade <parsasam1378@gmail.com>
2020-05-22 01:02:18 +0430

    Update Line.js

--- public/p5/start-screen/Line.js
+++ public/p5/start-screen/Line.js
@@ -3,7 +3,7 @@ function Line(i) {
   this.y = (height * 548) / 1080;
   let speed = -i * 0.5;
 
-  this.update = function() {
+  this.update = function () {
     speed += 0.1;
     if (speed >= 0) {
       this.y = this.y + speed;
@@ -14,10 +14,12 @@ function Line(i) {
     }
   };
 
-  this.show = function() {
+  this.show = function () {
     if (speed >= 0) {
       fill(255, 0, 0);
+      strokeWeight(2);
       line(this.x, this.y, width, this.y);
+      strokeWeight(1);
     }
   };
 }
